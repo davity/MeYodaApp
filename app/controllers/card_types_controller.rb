443149007d6f074f5d36c8eb73f305a3e4f0a687
David Morán <david.morandz@gmail.com>
2013-10-23 12:52:04 +0200

    Completado la gestión del modelo CardType (añadir, borrar...). Añadidos de vuelta los archivos de assets de css y javascript por errores al cargar las páginas. Activada de nuevo la opción para crear assets por defecto

--- app/controllers/card_types_controller.rb
+++ app/controllers/card_types_controller.rb
@@ -1,4 +1,49 @@
 class CardTypesController < ApplicationController
-	def new
+	def index
+		@card_types = CardType.all
 	end
+
+	def new
+		@card_type = CardType.new
+	end
+
+	def create
+		@card_type = CardType.new(card_type_params)
+
+		if @card_type.save
+			redirect_to @card_type
+		else
+			render 'edit'
+		end
+	end
+
+	def show
+		@card_type = CardType.find(params[:id])
+	end
+
+	def edit
+		@card_type = CardType.find(params[:id])
+	end
+
+	def update
+		@card_type = CardType.new(card_type_params)
+
+		if @card_type.save
+			redirect_to @card_type
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@card_type = CardType.find(params[:id])
+		@card_type.destroy
+
+		redirect_to card_types_path
+	end
+
+	private
+		def card_type_params
+			params.require(:card_type).permit(:name, :edition)
+		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 MeYodaApp::Application.routes.draw do
-  get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   #Resources
   resources :card_types
+
+  # Root
+  root to: "welcome#index"
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
