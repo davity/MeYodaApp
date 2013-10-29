8b36f6017e20cae07014288c5aa96b2a57099c5b
David Morán <david.morandz@gmail.com>
2013-10-29 12:47:19 +0100

    Terminados merges restantes que se me habían escapado y actualizado el schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131028094621) do
+ActiveRecord::Schema.define(version: 20131028095622) do
 
   create_table "card_types", force: true do |t|
     t.string   "name"
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(version: 20131028094621) do
   end
 
   add_index "cards", ["card_type_id"], name: "index_cards_on_card_type_id"
+
+  create_table "sales", force: true do |t|
+    t.float    "starting_price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "username"
