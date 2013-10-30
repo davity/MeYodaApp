13516655ff9f3c0cf29fea4b530150060d74addf
Javier Benito García-Mochales <javibgm@hotmail.com>
2013-10-30 17:59:32 +0100

    Initial view for buying cards

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
+  GEORGE_INDEX = 1.10
+  
   protected
   def authenticate_user
     # if there is a session key and the users id exists in the database we are autenticated
